56855d6eb78c28c2f87a1ee892ee6b1faa6d5669
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2023-11-08 02:52:15 +0600

    fetching complete for mybids & myjobs

--- src/pages/MyBids.jsx
+++ src/pages/MyBids.jsx
@@ -1,7 +1,35 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
+import Swal from "sweetalert2";
+
 const MyBids = () => {
+  const [myBids, setMyBids] = useState([]);
+  const {user} = useContext(AuthContext);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/mybids/${user.email}`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        setMyBids(data);
+      })
+    .catch(() => {
+        return Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong",
+          confirmButtonText: "Ok",
+        });
+      });
+  }, [user]);
+
   return (
     <div>
-      
+      <h1>{myBids.length}</h1>
     </div>
   );
 };
--- src/pages/MyJobs.jsx
+++ src/pages/MyJobs.jsx
@@ -1,9 +1,35 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../contexts/AuthProvider";
+import Swal from "sweetalert2";
 
 
 const MyJobs = () => {
+  const [myJobs, setMyJobs] = useState([]);
+  const {user} = useContext(AuthContext);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/myjobs/${user.email}`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        setMyJobs(data);
+      })
+    .catch(() => {
+        return Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong",
+          confirmButtonText: "Ok",
+        });
+      });
+  }, [user]);
   return (
     <>
-      
+      <h1>{myJobs.length}</h1>
     </>
   );
 };
