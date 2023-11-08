42523e62cba7456c2e032ad583be973e90f2f5ff
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2023-11-08 22:44:39 +0600

    minor update

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -4,7 +4,7 @@ const Faq = () => {
       id: 1,
       question: "How do I post a job listing?",
       answer:
-        'To post a job listing, log in to your account and click on the "Post a Job" button. Fill in the required details, such as job title, description, location, and requirements, and click "Submit."',
+        'To post a job listing, log in to your account and click on the "Add Jobs" button. Fill in the required details, such as job title, description, deadline, and category, and click "Submit."',
     },
     {
       id: 2,
@@ -15,7 +15,8 @@ const Faq = () => {
   ];
   return (
     <>
-      <div>
+      <div className="container mx-auto py-20">
+        <h1 className="text-xl md:text-3xl font-bold text-center pb-10">FAQ</h1>
         {faqData.map((item) => (
           <div key={item.id} className="collapse collapse-arrow bg-base-200">
             <input type="radio" name={`my-accordion-${item.id}`}/>
